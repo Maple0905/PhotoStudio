25c0fed614da307fb43be53a18f7005a62e64c1d
SuperStar27 <foreverNo1Techi@gmail.com>
2023-01-12 19:47:50 -0500

    complete about_us and what_we_do pages

--- resources/views/about_us.blade.php
+++ resources/views/about_us.blade.php
@@ -1,24 +1,34 @@
 @extends('layouts.app')
 
+@section('header')
+    @include('layouts.header_common')
+@endsection
+
 @section('content')
 
-<section class="wrapper bg-white">
-    <div class="container py-15 py-md-17">
-        <div class="row gy-10 gy-sm-13 gx-md-8 gx-xl-12 align-items-center mb-10 mb-md-12">
-            <div class="col-lg-6">
-                <div class="gx-md-5 gy-5">
-                    <figure class="rounded mx-md-5"><img src="{{ asset('img/about1.png') }}" srcset="{{ asset('img/about1.png 2x') }}" alt=""></figure>
-                </div>
+<section class="wrapper bg-light">
+    <div class="container pt-md-10 pb-md-10">
+        <div class="row col-lg-10 mx-auto align-items-center">
+            <div class="col-sm-6">
+                <img src="{{ asset('img/about1.png') }}" srcset="{{ asset('img/about1.png 2x') }}" style="width: 100%;" alt="" />
             </div>
-            <div class="col-lg-6">
-                <div class="border-dark">
-                    <h1 class="display-3 ls-sm mb-5">HERE TO MAKE AN IMPACT</h1>
-                    <p class="mb-6">Based in the vibrant city of London, Impact digital has earned itself the reputation of one of the leading photographic studios for providing an amazing photographic and digital experience.Working in conjunction with a renowned team of industry specialists (including photographers, make-up artists and hair stylists, retouchers) Our team are able to provide you with your own production team, studio set up and high definition products to suit your personal and business needs.</p>
+            <div class="col-sm-6">
+                <div class="border-dark mb-5">
+                    <h1 class="display-3 mb-0">HERE TO MAKE AN IMPACT</h1>
+                    <p class="mb-0">Based in the vibrant city of London, Impact digital has earned itself the reputation of one of the leading photographic studios for providing an amazing photographic and digital experience.Working in conjunction with a renowned team of industry specialists (including photographers, make-up artists and hair stylists, retouchers) Our team are able to provide you with your own production team, studio set up and high definition products to suit your personal and business needs.</p>
                 </div>
-                <div class="gx-md-5 gy-5">
-                    <figure class="rounded mx-md-5"><img src="{{ asset('img/about2.png') }}" srcset="{{ asset('img/about2.png 2x') }}" alt=""></figure>
-                </div>
+                <img src="{{ asset('img/about2.png') }}" srcset="{{ asset('img/about2.png 2x') }}" style="width: 100%;" alt="" />
             </div>
+        </div>
+    </div>
+</section>
+
+<section class="wrapper bg-light">
+    <div class="container pt-md-10 pb-md-5">
+        <div class="row col-lg-8 mx-auto text-center align-items-center">
+            <p>Many of our clients are repeat visitors to impact digital photographic studio. We take pride in every job we undertake and understand that your images and films need to engage That’s why we’ll never compromise on quality, either on the shoots themselves or during the editing process afterwards.</p>
+            <p>The majority of our work takes place in our studio but of course, we often conduct on-location shoots too.</p>
+            <p class="mb-0">So if you want a more accessible, professional and friendly approach, change your current photography supplier, or need commercial photography for the first time, we’d be happy to help.</p>
         </div>
     </div>
 </section>
--- resources/views/what_we_do.blade.php
+++ resources/views/what_we_do.blade.php
@@ -1,13 +1,17 @@
 @extends('layouts.app')
 
+@section('header')
+    @include('layouts.header_common')
+@endsection
+
 @section('content')
 
 <section class="wrapper">
-    <div class="container pt-12 pt-lg-8 pb-14 pb-md-17">
-        <div class="row text-center">
-            <div class="col-md-10 offset-md-1 col-lg-8 offset-lg-2 mb-10">
+    <div class="container pt-md-10 pb-md-10">
+        <div class="row col-lg-10 mx-auto text-center align-items-center">
+            <div class="col-md-6 offset-md-3 mb-10">
                 <div class="border-dark">
-                    <h1>WHAT WE DO</h1>
+                    <h1 class="mb-0">WHAT WE DO</h1>
                 </div>
             </div>
         </div>
@@ -17,7 +21,7 @@
                     <figure class="mb-6"><img class="img-fluid" src="{{ asset('img/product1.png') }}" srcset="{{ asset('img/product1.png 2x') }}" alt="" /></figure>
                     <h3>Studio Collaboration.</h3>
                     <p class="mb-2">We directly collaborate with amazing photographic studios within the UK, providing our own team of creatives.</p>
-                    <a href="/contact" class="more hover">Read More</a>
+                    <a href="/creatives" class="btn btn-dark custom-button">Read More</a>
                 </div>
             </div>
             <div class="col-lg-4">
@@ -25,7 +29,7 @@
                     <figure class="mb-6"><img class="img-fluid" src="{{ asset('img/product2.png') }}" srcset="{{ asset('img/product2.png 2x') }}" alt="" /></figure>
                     <h3>Our Products.</h3>
                     <p class="mb-2">We provide a range of products to suit our customers business and personal needs.</p>
-                    <a href="/contact" class="more hover">Read More</a>
+                    <a href="/creatives" class="btn btn-dark custom-button">Read More</a>
                 </div>
             </div>
             <div class="col-lg-4">
@@ -33,7 +37,7 @@
                     <figure class="mb-6"><img class="img-fluid" src="{{ asset('img/product3.png') }}" srcset="{{ asset('img/product3.png 2x') }}" alt="" /></figure>
                     <h3>Creative Team.</h3>
                     <p class="mb-2">We work with an incredible creative team that has a great passion for fashion.</p>
-                    <a href="/contact" class="more hover">Read More</a>
+                    <a href="/pricreativesces" class="btn btn-dark custom-button">Read More</a>
                 </div>
             </div>
         </div>
